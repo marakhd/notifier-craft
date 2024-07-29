08ae4f84810be1788ad266da2dc1f250478cacbd
marak <marakin09@mail.ru>
2024-07-29 16:07:06 +0300

    fix telegram sender

--- notifiercraft/sender/telegram.py
+++ notifiercraft/sender/telegram.py
@@ -5,8 +5,8 @@ class Telegram(TeleBot):
     def __init__(self, token: str, parse_mode: str=None, *args, **kwargs):
         super().__init__(token=token, parse_mode=parse_mode, *args, **kwargs)
         
-    # def send_message(self, chat_id, text):
-    #     super().send_message(chat_id=chat_id, text=text)
+    def send_message(self, chat_id, text, parse_mode="HTML", *args, **kwargs):
+        super().send_message(chat_id=chat_id, text=text, parse_mode=parse_mode, *args, **kwargs)
 
 
 class AsyncTelegram(Bot):
@@ -14,5 +14,5 @@ class AsyncTelegram(Bot):
         super().__init__(token=token, *args, **kwargs)
         
     async def send_message(self, chat_id, text, parse_mode="HTML", *args, **kwargs):
-        await super().send_message(chat_id=chat_id, text=text, *args, **kwargs)
+        await super().send_message(chat_id=chat_id, text=text, parse_mode=parse_mode, *args, **kwargs)
 
